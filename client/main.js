dc79e96829baeb1559db5ca5fb302afcfee820f7
Ruither Borba <ruither.borba@tecsinapse.com.br>
2016-06-18 11:47:14 -0300

    removendo pacotes pra teste no ambiente de prod

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
-import '../imports/startup/accounts-config.js';
 import '../imports/ui/corpo.js';
+import '../imports/startup/accounts-config.js';
 import '../imports/startup/routes.js';
 import '../imports/startup/google-maps.js';
 
